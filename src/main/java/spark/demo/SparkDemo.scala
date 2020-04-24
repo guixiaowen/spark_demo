ed050e8e32e8c4cbb58c9ad35182010307f31709
贵华雯 <guihuawen@corp.netease.com>
2020-04-24 19:13:02 +0800

    demo

--- src/main/java/spark/demo/SparkDemo.scala
+++ src/main/java/spark/demo/SparkDemo.scala
@@ -1,7 +1,5 @@
 package spark.demo
 
-import org.apache.hadoop.io.{LongWritable, Text}
-import org.apache.hadoop.mapred.TextInputFormat
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkDemo {
@@ -11,14 +9,8 @@ object SparkDemo {
     val sparkConf = new SparkConf().setMaster("local").setAppName("test")
     val sparkContext = new SparkContext(sparkConf)
     val distFile = sparkContext.textFile("/Users/guixiaowen/IdeaProjects/spark_demo/src/main/java/spark/demo/input/test.txt")
-
-    val distFile2 = sparkContext.hadoopFile("/Users/guixiaowen/IdeaProjects/spark_demo/src/main/java/spark/demo/input/test.txt", classOf[TextInputFormat], classOf[LongWritable], classOf[Text], 2)
-    // 高阶函数
-    // 等价于：x => x.split(" ") ｜
-    //
-    distFile.flatMap(_.split(" "))
-    // distFile.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).saveAsTextFile("/Users/guixiaowen/IdeaProjects/spark_demo/src/main/java/spark/demo/out")
-
+    val kvRdd = distFile.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    kvRdd.saveAsTextFile("/Users/guixiaowen/IdeaProjects/spark_demo/src/main/java/spark/demo/out")
     sparkContext.stop();
   }
 
